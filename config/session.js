8bdfb2adaf92db7819083bef55c6ba0b55f2e735
Miguel Tomairo <matomairo@gmail.com>
2020-04-11 19:16:39 -0500

    first commit

---
+++ config/session.js
@@ -0,0 +1,41 @@
+/**
+ * Session Configuration
+ * (sails.config.session)
+ *
+ * Use the settings below to configure session integration in your app.
+ * (for additional recommended settings, see `config/env/production.js`)
+ *
+ * For all available options, see:
+ * https://sailsjs.com/config/session
+ */
+
+module.exports.session = {
+
+  /***************************************************************************
+  *                                                                          *
+  * Session secret is automatically generated when your new app is created   *
+  * Replace at your own risk in production-- you will invalidate the cookies *
+  * of your users, forcing them to log in again.                             *
+  *                                                                          *
+  ***************************************************************************/
+  secret: 'd7ee0bd20c061fb0f5aa0fd92613dd3a',
+
+  // adapter: 'connect-mongo',
+  // url: 'mongodb+srv://rapser2:scctqmmt@cluster0-srpbc.mongodb.net/miguel?retryWrites=true&w=majority',
+  // collection: 'sessions',
+  /***************************************************************************
+  *                                                                          *
+  * Customize when built-in session support will be skipped.                 *
+  *                                                                          *
+  * (Useful for performance tuning; particularly to avoid wasting cycles on  *
+  * session management when responding to simple requests for static assets, *
+  * like images or stylesheets.)                                             *
+  *                                                                          *
+  * https://sailsjs.com/config/session                                       *
+  *                                                                          *
+  ***************************************************************************/
+  // isSessionDisabled: function (req){
+  //   return !!req.path.match(req._sails.LOOKS_LIKE_ASSET_RX);
+  // },
+
+};
